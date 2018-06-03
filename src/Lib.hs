4784041546e2d15fe88065a74c25767a51e94c65
Eric Conlon <econlon@lyft.com>
2018-06-03 15:01:09 -0700

    more

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,50 +1,124 @@
+{-# LANGUAGE
+  DeriveFoldable,
+  DeriveGeneric,
+  DeriveTraversable,
+  ViewPatterns,
+  FlexibleContexts
+#-}
+
 module Lib where
 
-data Term =
+import Control.Monad (MonadPlus(..))
+import Control.Monad.Except (MonadError(..))
+import Data.Fix (Fix(..))
+-- import Data.Sequence (Seq)
+-- import Data.Text (Text)
+-- import Data.Text as T
+import GHC.Generics (Generic)
+
+pureFix :: Applicative m => f (Fix f) -> m (Fix f)
+pureFix = pure . Fix
+
+data Term a =
     TmTrue
   | TmFalse
-  | TmIf Term Term Term
+  | TmIf a a a
   | TmZero
-  | TmSucc Term
-  | TmPred Term
-  | TmIsZero Term
-  deriving (Show, Eq)
+  | TmSucc a
+  | TmPred a
+  | TmIsZero a
+  -- | TmLam Text a
+  -- | TmApp a a
+  deriving (Show, Eq, Functor, Foldable, Traversable, Generic)
 
-isNumericVal :: Term -> Bool
-isNumericVal TmZero = True
-isNumericVal (TmSucc t) = isNumericVal t
-isNumericVal _ = False
+type FixTerm = Fix Term
 
-isVal :: Term -> Bool
-isVal TmTrue = True
-isVal TmFalse = True
-isVal t = isNumericVal t
+data Type a =
+    TyBool
+  | TyNat
+  -- | TyFun a a
+  deriving (Show, Eq, Functor, Foldable, Traversable, Generic)
 
-smallStep :: Term -> Maybe Term
+type FixType = Fix Type
+
+isNumericVal :: FixTerm -> Bool
+isNumericVal t =
+  case unFix t of
+    TmZero -> True
+    TmSucc t' -> isNumericVal t'
+    _ -> False
+
+isBoolVal :: FixTerm -> Bool
+isBoolVal t =
+  case unFix t of
+    TmTrue -> True
+    TmFalse -> True
+    _ -> False
+
+isVal :: FixTerm -> Bool
+isVal = (||) <$> isNumericVal <*> isBoolVal
+
+smallStep :: MonadPlus m => FixTerm -> m FixTerm
 smallStep t =
-  case t of
-    TmIf TmTrue t2 _ -> return t2
-    TmIf TmFalse _ t3 -> return t3
+  case unFix t of
+    TmIf (unFix -> TmTrue) t2 _ -> return t2
+    TmIf (unFix -> TmFalse) _ t3 -> return t3
     TmIf t1 t2 t3 -> do
       t1' <- smallStep t1
-      return (TmIf t1' t2 t3)
+      pureFix (TmIf t1' t2 t3)
     TmSucc t1 -> do
       t1' <- smallStep t1
-      return (TmSucc t1')
-    TmPred TmZero -> return TmZero
-    TmPred (TmSucc t1) | isNumericVal t1 -> return t1
+      pureFix (TmSucc t1')
+    TmPred (unFix -> TmZero) -> pureFix TmZero
+    TmPred (unFix -> TmSucc t1) | isNumericVal t1 -> pure t1
     TmPred t1 -> do
       t1' <- smallStep t1
-      return (TmPred t1')
-    TmIsZero TmZero -> return TmTrue
-    TmIsZero (TmSucc t1) | isNumericVal t1 -> return TmFalse
+      pureFix (TmPred t1')
+    TmIsZero (unFix -> TmZero) -> pureFix TmTrue
+    TmIsZero (unFix -> TmSucc t1) | isNumericVal t1 -> pureFix TmFalse
     TmIsZero t1 -> do
       t1' <- smallStep t1
-      return (TmIsZero t1')
-    _ -> Nothing
+      pureFix (TmIsZero t1')
+    _ -> mzero
 
-evaluate :: Term -> Term
+evaluate :: FixTerm -> FixTerm
 evaluate t =
   case smallStep t of
     Just t' -> evaluate t'
     Nothing -> t
+
+data TypeErr =
+    Boom
+  | CannotUnify FixType FixType
+  deriving (Show, Eq, Generic)
+
+unifyTy :: MonadError TypeErr m => FixType -> FixType -> m FixType
+unifyTy t1 t2 =
+    case (unFix t1, unFix t2) of
+      (TyBool, TyBool) -> pureFix TyBool
+      (TyNat, TyNat) -> pureFix TyNat
+      _ -> throwError (CannotUnify t1 t2)
+
+checkType :: MonadError TypeErr m => FixType -> FixTerm -> m ()
+checkType _ _ = undefined
+
+inferType :: MonadError TypeErr m => FixTerm -> m FixType
+inferType t =
+  case unFix t of
+    TmTrue -> pureFix TyBool
+    TmFalse -> pureFix TyBool
+    TmIf t1 t2 t3 -> do
+      checkType (Fix TyBool) t1
+      thenTy <- inferType t2
+      elseTy <- inferType t3
+      unifyTy thenTy elseTy
+    TmZero -> pureFix TyNat
+    TmSucc t1 -> do
+      checkType (Fix TyNat) t1
+      pureFix TyNat
+    TmPred t1 -> do
+      checkType (Fix TyNat) t1
+      pureFix TyNat
+    TmIsZero t1 -> do
+      checkType (Fix TyNat) t1
+      pureFix TyBool
